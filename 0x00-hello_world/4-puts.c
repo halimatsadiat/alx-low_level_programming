6244f0e7b955cb49b35e11d6ee74e155879a1534
halimatsadiat <Haleymaogidan@gmail.com>
2022-09-08 13:19:13 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include <string.h>
+
+
+
+/**
+ *
+ *  *  * main - Entry point
+ *
+ *   *   *
+ *
+ *    *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char myChar[55];
+
+
+
+			strcpy(myChar, "\"Programming is like building a multilingual puzzle");
+
+
+
+				puts(myChar);
+
+
+
+					return (0);
+
+}
