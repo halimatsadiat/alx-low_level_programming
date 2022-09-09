494dedf454beca033a6c27e9cc9751594d694576
halimatsadiat <Haleymaogidan@gmail.com>
2022-09-08 18:01:45 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,15 +2,13 @@
 
 #include <string.h>
 
-
-
 /**
+ 
+ * main - Entry point
  *
- *  *  * main - Entry point
- *
- *    *    * Return: Always 0 (Success)
- *
- **/
+ * Return: Always 0 (Success)
+ 
+ */
 
 int main(void)
 
