d3b48ddff60dda113e56b0f59c3296c52216af8d
halimatsadiat <Haleymaogidan@gmail.com>
2022-09-09 21:14:24 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+	printf("\nLowercase characters : \n");
+	    for (ch = 'a'; ch <= 'z'; ch++)
+	    {
+		    printf("%c ", ch);
+	    }
+	    return (0);
+}
