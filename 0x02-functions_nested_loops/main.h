82dfa1bb96e42201b5a176ce3b2c3a26296c0ff9
halimatsadiat <Haleymaogidan@gmail.com>
2022-09-13 20:14:10 -0700

    task 6 and 7

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -7,7 +7,7 @@ void print_alphabet_x10(void);
 int _islower(int ch);
 int _isalpha(int ch);
 int print_sign(int x);
-int abs(int n);
-int print_last_digit(int n);
+int _abs(int);
+int print_last_digit(int);
 
 #endif 
