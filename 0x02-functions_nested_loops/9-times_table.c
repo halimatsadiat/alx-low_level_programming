4d94e810021bbccb0519aa77a3ba72d156c6f841
halimatsadiat <Haleymaogidan@gmail.com>
2022-09-14 14:47:25 -0700

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,6 +12,8 @@ void times_table(void)
 		for (mul = 0; mul <= 9; mul++)
 		{
 			nineTimes = nums * mul;
+			if (nineTimes > 9)
+				_putchar((nineTimes / 10) + '0');
 			_putchar((nineTimes % 10) + '0');
 			_putchar(',');
 			_putchar(' ');
