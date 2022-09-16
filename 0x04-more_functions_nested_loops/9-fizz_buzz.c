d5106cb487876a69665c4f4568a25095c2298b98
halimatsadiat <Haleymaogidan@gmail.com>
2022-09-15 17:46:28 -0700

    task fizz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry point
  *
- * Return: 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
@@ -12,27 +12,22 @@ int main(void)
 	for (num = 1; num <= 100; num++)
 	{
 		if ((num % 3) == 0 && (num % 5) == 0)
-		{
 			printf("FizzBuzz");
-		}
+
 		else if ((num % 3) == 0)
-		{
 			printf("Fizz");
-		}
+
 		else if ((num % 5) == 0)
-		{
 			printf("Buzz");
-		}
+
 		else
-		{
 			printf("%d", num);
-		}
+
 		if (num == 100)
-		{
 			continue;
-		}
 		printf(" ");
 	}
-	printf('\n');
+	printf("\n");
+
 	return (0);
 }
