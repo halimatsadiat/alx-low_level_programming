c7d4d0210ea71a7699ddb3eeffa1fd2ab40d06e6
halimatsadiat <Haleymaogidan@gmail.com>
2022-09-19 10:55:16 -0700

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string to be reverse
+ */
+void rev_string(char *s)
+{
+	int len = 0, index = 0;
+	char x;
+
+	while (s[index++])
+	{
+		len++;
+	}
+
+	for (index = len - 1; index >= len / 2; index--)
+	{
+		x = s[index];
+		s[index] = s[len - index - 1];
+		[len - index - 1] = x;
+	}
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ void swap_int(int *a, int *b);
 int _strlen(char *str);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 
 #endif
