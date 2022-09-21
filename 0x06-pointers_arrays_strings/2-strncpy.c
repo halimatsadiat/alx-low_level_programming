3fd4f63c88dda6d6a72660cd9cf6ddbe9242e2b6
halimatsadiat <Haleymaogidan@gmail.com>
2022-09-21 14:46:36 -0700

    task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a number
+ * @dest: buffer
+ * @src: source
+ *
+ * Return: a pointer to dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0, k = 0;
+
+	while (src[i++])
+	{
+		k++;
+	}
+
+	for (i = 0; src[i] && i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	for (i = k; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
+
+	return (dest);
+}
