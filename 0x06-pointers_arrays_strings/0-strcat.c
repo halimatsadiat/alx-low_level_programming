499f86b9419698de88ffc8de519905abc7edd753
halimatsadiat <Haleymaogidan@gmail.com>
2022-09-22 10:17:28 -0700

    task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,14 +10,12 @@
 char *_strcat(char *dest, char *src)
 {
 	int i, j;
-	i = 0;
 
-	while (dest[i] != '\0')
-	{
-		i++
-	}
 
-	for (j = 0; src[j] != '\0'; j++)
+	for (i = 0; dest[i] != '\0'; i ++)
+		;
+
+	for (j = 0; src[j] != '\0'; j++, i++)
 	{
 		dest[i] = src[j];
 	}
