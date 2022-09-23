73ae071a5ccda387ca60d2724a7da9a035b2cf3f
halimatsadiat <Haleymaogidan@gmail.com>
2022-09-23 08:03:34 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,14 +10,13 @@ char *cap_string(char *str)
 {
 	int i = 0;
 
-	for (i =0; str[i] != '\0'; i++)
+	while (str[i])
 	{
 		while ((!str[i] >= 'a' && str[i] <= 'z'))
 			i++;
 		if (str[i - 1]  == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' || str[i - 1] == ',' || str[i] == ';' || str[i - 1] == '.' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}' || str[i - 1] == 0)
-		{
 			str[i] = (int)str[i] - 32;
-		}
+		i++;
 	}
 	return (str);
 }
