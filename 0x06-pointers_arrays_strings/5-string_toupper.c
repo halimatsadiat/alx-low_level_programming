ebb480a68dfa919a69d676aa24a0ca04c59bc36b
halimatsadiat <Haleymaogidan@gmail.com>
2022-09-23 07:05:01 -0700

    convert to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,12 +8,12 @@
  */
 char *string_toupper(char *str)
 {
-	int i = 0;
+	int i;
 
-	for (str[i] >= 'a'; str[i] <= 'z'; str[i++])
+	for(i = 0; str[i] != '\0'; i++)
 	{
-		str[i] -= 32;
+		if (str[i] >= 'a' && str[i] <= 'z')
+			str[i] = (int)str[i] - 32;
 	}
-
 	return (str);
 }
