89f00e1c536130e6df82ef69ef98c3a080ed52a0
halimatsadiat <Haleymaogidan@gmail.com>
2022-09-27 20:28:29 -0700

    task 0,5,6

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strstr - locate a substring
+ * @haystack: string to be searched
+ * @needle: the substring
+ *
+ * Return: if substring is located- apointer to the begining, else - NULL
+ */
+char *strstr(char *haystack, char *needle)
+{
+	int i;
+
+	if (*needle == 0)
+		return (haystack);
+
+	while (*haystack)
+	{
+		i = 0;
+
+		if (haystack[i] == needle[i])
+		{
+			do {
+				if (needle[i + 1] == '\0')
+					return (haystack);
+				i++;
+			}
+			while (haystack[i] == needle[i]);
+		}
+		haystack++;
+	}
+	return ('\0');
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,8 @@ void *_memset(void *s, int c, size_t n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+void print_chestboard(char (*a)[8]);
 
 #endif
