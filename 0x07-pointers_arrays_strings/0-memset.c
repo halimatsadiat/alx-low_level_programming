d306bff190bb9c2e8cd28a059dcfdfb3815c554b
halimatsadiat <Haleymaogidan@gmail.com>
2022-09-27 20:31:36 -0700

    task 0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,7 +13,7 @@ void *_memset(void *s, int c, size_t n)
 	unsigned int i;
 	unsigned char *memory = s, value = c;
 
-	for (i = 0; i < n; i+++)
+	for (i = 0; i < n; i++)
 	{
 		memory[i] = value;
 	}
