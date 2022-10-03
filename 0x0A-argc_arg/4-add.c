9a272b13fe410637a1aa744ba7ef95fe04ea5cc9
halimatsadiat <Haleymaogidan@gmail.com>
2022-10-03 05:30:21 -0700

    task 0,1,2,3,4

---
+++ 0x0A-argc_arg/4-add.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the addition of positive number
+ * @argc: argument count
+ * @argv: array of pointers
+ *
+ * Return: success 1, else 0
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, sum = 0;
+
+	for (a = 1; a < argc; a++)
+	{
+		for (b = 0; argv[a][b]; b++)
+		{
+			if (argv[a][b] < '0' || argv[a][b] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argc[a]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
