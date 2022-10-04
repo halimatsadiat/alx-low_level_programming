2ad8a9fb5bee27d55c1ac5e1d354601cea904ffb
halimatsadiat <Haleymaogidan@gmail.com>
2022-10-04 01:41:07 -0700

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,8 +15,11 @@ int **alloc_grid(int width, int height)
 	int **twoDi;
 	int height_i, width_i;;
 
-	if (width <= 0 || height <= 0)
+	if (width <= 0)
 		return (NULL);
+
+	if (height <= 0)
+		retrun (NULL);
 
 	twoDi = malloc(sizeof(int *) * height);
 
