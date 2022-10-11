0d355cb50591072828a2c061a7f75c6807698ce4
halimatsadiat <Haleymaogidan@gmail.com>
2022-10-10 21:10:19 -0700

    task 3

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,33 +46,33 @@ char *_strcopy(char *dest, char *src)
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *doggy;
+	dog_t *doggo;
 
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
 
-	doggy = malloc(sizeof(dog_t));
-	if (doggy == NULL)
+	doggo = malloc(sizeof(dog_t));
+	if (doggo == NULL)
 		return (NULL);
 
-	doggy->name = malloc(sizeof(char) * (_strlen(name) + 1));
-	if (doggy-> name == NULL)
+	doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (doggo-> name == NULL)
 	{
-		free(doggy);
+		free(doggo);
 		return (NULL);
 	}
 
-	doggy->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-	if (doggy->owner == NULL)
+	doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (doggo->owner == NULL)
 	{
-		free(doggy->name);
-		free(doggy);
+		free(doggo->name);
+		free(doggo);
 		return (NULL);
 	}
 
-	doggy->name = _strcopy(doggy->name, name);
-	doggy->age = age;
-	doggy->owner = _strcopy(doggy->owner , owner);
+	doggo->name = _strcopy(doggo->name, name);
+	doggo->age = age;
+	doggo->owner = _strcopy(doggo->owner , owner);
 
 	return (doggy);
 }
