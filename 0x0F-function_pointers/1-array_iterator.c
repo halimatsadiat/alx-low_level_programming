5e205bfeac27967235f4c6fcd0fc1ff666259e93
halimatsadiat <Haleymaogidan@gmail.com>
2022-10-12 18:35:12 -0700

    task 1,2 ,3

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,8 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - Executes a function given as a parameter on each new element
+ * array_iterator - Executes a function given as
+ * a parameter on each new element
  * @array: the array
  * @size: size of the array
  * @action: pointer to the function to be executed
